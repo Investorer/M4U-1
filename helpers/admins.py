427d1d97f3bdf89ed0e78aa4c600d0fd61e5ddee
Investorer <85453613+Investorer@users.noreply.github.com>
2021-07-12 07:59:08 +0530

    Update admins.py

--- helpers/admins.py
+++ helpers/admins.py
@@ -1,20 +1,3 @@
-# Calls Music 1 - Telegram bot for streaming audio in group calls
-# Copyright (C) 2021  Roj Serbest
-
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-
 from typing import List
 
 from pyrogram.types import Chat
@@ -22,8 +5,8 @@ from pyrogram.types import Chat
 import cache.admins
 
 
-async def get_administrators(chat: Chat) -> List[int]:
-    get = cache.admins.get(chat.id)
+async def get_administrators(chat: Chat) -> List[User]:
+    get = gett(chat.id)
 
     if get:
         return get
@@ -32,8 +15,7 @@ async def get_administrators(chat: Chat) -> List[int]:
         to_set = []
 
         for administrator in administrators:
-            if administrator.can_manage_voice_chats:
-                to_set.append(administrator.user.id)
+            # if administrator.can_manage_voice_chats:
+            to_set.append(administrator.user.id)
 
-        cache.admins.set(chat.id, to_set)
-        return await get_administrators(chat)
+        set(chat.id, to_set)
